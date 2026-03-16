026c65e89d87ff936257c577a8c229dc5430f255
brymmobaggins <ibro2tablet21@yahoo.com>
2026-03-16 13:21:11 +0100

    feat: add color to react icons

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -7,33 +7,33 @@ export default function Sidebar() {
   return (
     <>
       <div
-        className="flex flex-col items-center border fixed left-0 z-1 h-full w-40
-      overflow-hidden pt-5
-      *:text-gray-500 *:p-1.5 *:hover:bg-gray-100 *:rounded *:list-none"
+        className="items-center border fixed left-0 z-1 h-full w-50
+      overflow-hidden pt-5 p-1.5
+      *:text-gray-500 *:p-1.5 *:hover:bg-gray-100 *:rounded *:list-none *:flex"
       >
-        <li className="flex items-center gap-4">
+        <li className="items-center gap-4">
           <FaBookOpen size={18} style={{ color: "black" }} />
           <a href="">Note</a>
         </li>
 
-        <li className="flex items-center gap-4">
-          <FaBell size={18} />
+        <li className="items-center gap-4">
+          <FaBell size={18} style={{ color: "black" }} />
           <a href="">Reminder</a>
         </li>
-        <li className="flex items-center gap-4">
-          <FaPen size={18} />
+        <li className="items-center gap-4">
+          <FaPen size={18} style={{ color: "black" }} />
           <a href="" className="float-left">
             Edit label
           </a>
         </li>
-        <li className="flex items-center gap-4">
-          <FaFileArchive size={18} />
+        <li className="items-center gap-4">
+          <FaFileArchive size={18} style={{ color: "black" }} />
           <a href="" className="float-left">
             Archieve
           </a>
         </li>
-        <li className="flex items-center gap-4">
-          <FaTrashCan size={18} />
+        <li className="items-center gap-4">
+          <FaTrashCan size={18} style={{ color: "black" }} />
           <a href="" className="float-left">
             Trash
           </a>
